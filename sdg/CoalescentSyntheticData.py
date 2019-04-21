b8274faa2208bc3ec13330c43978a3f9b1043516
Eric Auel <eauel@vols.utk.edu>
2019-04-21 16:29:03 -0400

    Switch to using Zarr to store data directly (in chunks) to avoid killing system memory

--- sdg/CoalescentSyntheticData.py
+++ sdg/CoalescentSyntheticData.py
@@ -1,7 +1,6 @@
 import msprime
-import numpy as np
-import dask.array as da
-from sdg.DaskUtils import *
+import zarr
+from numcodecs import Blosc
 
 ZARR_PATH = './output.zarr'
 
@@ -11,13 +10,6 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
     """
         Function credits: Nick Harding
         Reference URL: https://hardingnj.github.io/2017/08/23/power-of-correct-tools.html
-
-    tree_sequence = msprime.simulate(population_configurations=pop_cfg,
-                                     demographic_events=dem_hist,
-                                     length=num_bases,
-                                     recombination_rate=rrate,
-                                     mutation_rate=mu,
-                                     random_seed=seed)
     """
     tree_sequence = msprime.simulate(
         sample_size=num_samples * ploidy, Ne=Ne, length=num_bases, recombination_rate=rrate, mutation_rate=mu,
@@ -26,48 +18,40 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
     # Print the number of mutations in tree sequence
     print("Simulated ", tree_sequence.get_num_mutations(), "mutations")
 
-    V = None
+    print("Creating Zarr data store root")
+    store = zarr.DirectoryStore(ZARR_PATH)
+    root = zarr.group(store=store, overwrite=True)
 
-    print("Pulling variant data...")
+    print('Creating Zarr Array')
+    compressor = Blosc(cname='zstd', clevel=1, shuffle=Blosc.AUTOSHUFFLE)
+    z = root.empty('calldata/GT', shape=(tree_sequence.get_num_mutations(), num_samples, ploidy),
+                   chunks=(2048, 2048, ploidy),
+                   dtype='i1',
+                   compressor=compressor)
 
-    for variant in tree_sequence.variants():
-        var = np.zeros(shape=(1, variant.genotypes.shape[0]))
-        var[0, :] = variant.genotypes
-        if V is not None:
-            V = da.concatenate([V, var], axis=0)
-        else:
-            V = var
-            # V = V.reshape((-1, 1))
-
-    del tree_sequence
-
-    print("  - Done.")
-
-    # Reshape data
-    newshape = (V.shape[0], -1, ploidy)
-    V = V.reshape(newshape)
-
-    num_variants = V.shape[0]
-    num_samples = V.shape[1]
-    num_ploidy = V.shape[2]
+    num_variants = z.shape[0]
+    num_samples = z.shape[1]
+    num_ploidy = z.shape[2]
 
     print('Num Samples: {}'.format(num_samples))
     print('Num Variants: {}'.format(num_variants))
     print('Ploidy: {}'.format(num_ploidy))
     print("Variation rate: {}".format(num_variants / num_bases))
 
-    # Save the genotype data to zarr store
-    print('Saving data to zarr directory')
-    gt_output = V.rechunk(chunks=(65536, 64, ploidy))
-    da.to_zarr(arr=gt_output, url=ZARR_PATH, component='calldata/GT', overwrite=True)
+    print("Pulling variant data...")
+    variant_counter = 0
+    for variant in tree_sequence.variants():
+        print("  Variant {} out of {}".format(variant_counter, tree_sequence.get_num_mutations()))
+        var = variant.genotypes.reshape((num_samples, ploidy))
+        z[variant.index, :, :] = var
+        variant_counter += 1
 
-    print('done')
+    print('Done.\n')
+    print(z.info)
 
 
 if __name__ == '__main__':
-    connect_dask(address='127.0.0.1', port=8786)
-
-    generate_coalescent_synthetic_data(num_samples=2548,
+    generate_coalescent_synthetic_data(num_samples=100000,
                                        num_bases=50e6,
                                        Ne=1e4,
                                        mu=3.5e-9,
