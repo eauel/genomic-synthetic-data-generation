a2de600810aec5a4b1cb3c7248cd30a7e4217eae
Eric Auel <eauel@vols.utk.edu>
2019-04-21 14:27:32 -0400

    Change simulation method Switch to Dask array instead of Numpy array for storing variant data

--- sdg/CoalescentSyntheticData.py
+++ sdg/CoalescentSyntheticData.py
@@ -6,61 +6,68 @@ import dask.array as da
 ZARR_PATH = './output.zarr'
 
 
-def generate_coalescent_synthetic_data(pop_cfg, dem_hist, num_bases=1e7, mu=3.5e-9, rrate=1e-8,
+def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, mu=3.5e-9, rrate=1e-8,
                                        ploidy=2, seed=57):
     """
         Function credits: Nick Harding
         Reference URL: https://hardingnj.github.io/2017/08/23/power-of-correct-tools.html
-    """
+
     tree_sequence = msprime.simulate(population_configurations=pop_cfg,
                                      demographic_events=dem_hist,
                                      length=num_bases,
                                      recombination_rate=rrate,
                                      mutation_rate=mu,
                                      random_seed=seed)
+    """
+    tree_sequence = msprime.simulate(
+        sample_size=num_samples * ploidy, Ne=Ne, length=num_bases, recombination_rate=rrate, mutation_rate=mu,
+        random_seed=seed)
 
     # Print the number of mutations in tree sequence
     print("Simulated ", tree_sequence.get_num_mutations(), "mutations")
 
-    V = np.zeros(shape=(tree_sequence.get_num_mutations(), tree_sequence.get_sample_size()), dtype=np.int8)
+    V = None
+
+    print("Pulling variant data...")
 
     for variant in tree_sequence.variants():
-        V[variant.index] = variant.genotypes
+        var = np.zeros(shape=(1, variant.genotypes.shape[0]))
+        var[0, :] = variant.genotypes
+        if V is not None:
+            V = da.concatenate([V, var], axis=0)
+        else:
+            V = var
+            # V = V.reshape((-1, 1))
 
-    ht = allel.HaplotypeArray(V)
-    gt = ht.to_genotypes(ploidy=ploidy)
+    del tree_sequence
 
-    print('Num Allele Calls: {}'.format(gt.n_allele_calls))
-    print('Num Calls: {}'.format(gt.n_calls))
-    print('Num Samples: {}'.format(gt.n_samples))
-    print('Num Variants: {}'.format(gt.n_variants))
+    print("  - Done.")
 
-    gt = gt.values  # Get underlying NumPy array
+    # Reshape data
+    newshape = (V.shape[0], -1, ploidy)
+    V = V.reshape(newshape)
+
+    num_variants = V.shape[0]
+    num_samples = V.shape[1]
+    num_ploidy = V.shape[2]
+
+    print('Num Samples: {}'.format(num_samples))
+    print('Num Variants: {}'.format(num_variants))
+    print('Ploidy: {}'.format(num_ploidy))
+    print("Variation rate: {}".format(num_variants / num_bases))
 
     # Save the genotype data to zarr store
     print('Saving data to zarr directory')
-    gt_output = da.from_array(gt, chunks=(65536, 64, ploidy))
+    gt_output = V.rechunk(chunks=(65536, 64, ploidy))
     da.to_zarr(arr=gt_output, url=ZARR_PATH, component='calldata/GT', overwrite=True)
 
     print('done')
 
 
 if __name__ == '__main__':
-    pop_history = [
-        msprime.PopulationParametersChange(time=20, growth_rate=-0.25, population_id=0),
-        msprime.PopulationParametersChange(time=40, growth_rate=-0.004, population_id=0),
-        msprime.PopulationParametersChange(time=1000, growth_rate=0, population_id=0)]
-
-    pop_config = [msprime.PopulationConfiguration(
-        sample_size=100000 * 2, initial_size=1000, growth_rate=0)]
-
-    dp = msprime.DemographyDebugger(population_configurations=pop_config,
-                                    demographic_events=pop_history)
-    dp.print_history()
-
-    generate_coalescent_synthetic_data(pop_config,
-                                       pop_history,
+    generate_coalescent_synthetic_data(num_samples=2548,
                                        num_bases=50e6,
+                                       Ne=1e4,
                                        mu=3.5e-9,
                                        rrate=1e-8,
                                        ploidy=2,
