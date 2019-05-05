282be7aa7a65fda875757316029d55adc90f6f18
Eric Auel <eauel@vols.utk.edu>
2019-05-05 18:24:32 -0400

    Add progressbar to monitor progress (requires etaprogress package) Modify chunking size to greatly speed up the writing to disk time

--- sdg/CoalescentSyntheticData.py
+++ sdg/CoalescentSyntheticData.py
@@ -1,8 +1,11 @@
 import msprime
 import zarr
 from numcodecs import Blosc
+from etaprogress.progress import ProgressBar
+import sys
 
 ZARR_PATH = './output.zarr'
+VARIANTS_PER_CHUNK = 64
 
 
 def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, mu=3.5e-9, rrate=1e-8,
@@ -24,8 +27,10 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
 
     print('Creating Zarr Array')
     compressor = Blosc(cname='zstd', clevel=1, shuffle=Blosc.AUTOSHUFFLE)
-    z = root.empty('calldata/GT', shape=(tree_sequence.get_num_mutations(), num_samples, ploidy),
-                   chunks=(2048, 2048, ploidy),
+    z_shape = (tree_sequence.get_num_mutations(), num_samples, ploidy)
+    z_chunks = (VARIANTS_PER_CHUNK, z_shape[1], z_shape[2])
+    z = root.empty('calldata/GT', shape=z_shape,
+                   chunks=z_chunks,
                    dtype='i1',
                    compressor=compressor)
 
@@ -38,10 +43,14 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
     print('Ploidy: {}'.format(num_ploidy))
     print("Variation rate: {}".format(num_variants / num_bases))
 
+    bar = ProgressBar(tree_sequence.get_num_mutations(), max_width=80)
     print("Pulling variant data...")
     variant_counter = 0
     for variant in tree_sequence.variants():
-        print("  Variant {} out of {}".format(variant_counter, tree_sequence.get_num_mutations()))
+        bar.numerator = variant_counter
+        print(bar, end='\r')
+        sys.stdout.flush()
+
         var = variant.genotypes.reshape((num_samples, ploidy))
         z[variant.index, :, :] = var
         variant_counter += 1
