762223b1c3b2b6ceadd3ba226595d92051952107
Eric Auel <eauel@vols.utk.edu>
2019-08-06 20:21:27 -0400

    Don't store everything in memory, change chunk size

--- sdg/CoalescentSyntheticData.py
+++ sdg/CoalescentSyntheticData.py
@@ -5,13 +5,9 @@ from etaprogress.progress import ProgressBar
 import sys
 
 ZARR_PATH = './output.zarr'
-DS_VARIANTS_PER_CHUNK = 16384
-DS_SAMPLES_PER_CHUNK = 16384
-DS_PLOIDY_PER_CHUNK = None
-
-MEM_VARIANTS_PER_CHUNK = 1
-MEM_SAMPLES_PER_CHUNK = None
-MEM_PLOIDY_PER_CHUNK = None
+VARIANTS_PER_CHUNK = 1
+SAMPLES_PER_CHUNK = None
+PLOIDY_PER_CHUNK = None
 
 
 def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, mu=3.5e-9, rrate=1e-8,
@@ -39,7 +35,7 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
     print('Creating Zarr Array')
     compressor = Blosc(cname='zstd', clevel=1, shuffle=Blosc.AUTOSHUFFLE)
     z_shape = (tree_sequence.get_num_mutations(), num_samples, ploidy)
-    z_chunks = (DS_VARIANTS_PER_CHUNK, DS_SAMPLES_PER_CHUNK, DS_PLOIDY_PER_CHUNK)
+    z_chunks = (VARIANTS_PER_CHUNK, SAMPLES_PER_CHUNK, PLOIDY_PER_CHUNK)
     z = root.empty('calldata/GT', shape=z_shape,
                    chunks=z_chunks,
                    dtype='i1',
@@ -56,8 +52,6 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
 
     bar = ProgressBar(tree_sequence.get_num_mutations(), max_width=80)
     print("Pulling variant data...")
-    z_temp_chunks = (MEM_VARIANTS_PER_CHUNK, MEM_SAMPLES_PER_CHUNK, MEM_PLOIDY_PER_CHUNK)
-    z_temp = zarr.zeros(shape=z_shape, compressor=compressor, chunks=z_temp_chunks)
     variant_counter = 0
     for variant in tree_sequence.variants():
         bar.numerator = variant_counter
@@ -65,11 +59,8 @@ def generate_coalescent_synthetic_data(num_samples=1000, num_bases=1e7, Ne=1e4, 
         sys.stdout.flush()
 
         var = variant.genotypes.reshape((num_samples, ploidy))
-        z_temp[variant.index, :, :] = var
+        z[variant.index, :, :] = var
         variant_counter += 1
-
-    # Update the Zarr directory store with the data in memory
-    z[:, :, :] = z_temp
 
     print('Done.\n')
     print(z.info)
